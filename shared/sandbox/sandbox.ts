37e9a9f46ed06dc3064a34c9a6ed23c30bed94fd
Berk Cambaz <berkcambaz12321@gmail.com>
2022-03-11 16:49:20 +0300

    Implement constructors for sandbox

--- shared/sandbox/sandbox.ts
+++ shared/sandbox/sandbox.ts
@@ -1,15 +1,19 @@
+import { Vec2 } from "../core/vec2";
 import { Ball } from "./ball";
 import { Paddle } from "./paddle";
 
 export class Sandbox {
+  public readonly WIDTH = 1280;
+  public readonly HEIGHT = 720;
+
   public paddleRight: Paddle;
   public paddleLeft: Paddle;
   public ball: Ball;
 
   constructor() {
-    this.paddleRight = new Paddle();
-    this.paddleLeft = new Paddle();
-    this.ball = new Ball();
+    this.paddleRight = new Paddle(new Vec2(0, 0), new Vec2(10, 50));
+    this.paddleLeft = new Paddle(new Vec2(0, 0), new Vec2(10, 50));
+    this.ball = new Ball(new Vec2(0, 0), new Vec2(10, 10));
   }
 
   public tick() {
