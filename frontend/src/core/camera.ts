07c5fc081b1d2ef4847d0065c917aa7e4f0cbcee
Berk Cambaz <berkcambaz12321@gmail.com>
2022-03-11 16:12:17 +0300

    Implement camera onResize & add canvas styling

--- frontend/src/core/camera.ts
+++ frontend/src/core/camera.ts
@@ -16,6 +16,18 @@ export class Camera {
   }
 
   private onResize(w: number, h: number) {
+    // TODO: Handle for different device pixel ratios
 
+    let target = this.w / this.h;
+    let current = w / h;
+
+    if (current > target) w = h * (16 / 9);
+    else h = w * (9 / 16);
+
+    game.canvas.width = w;
+    game.canvas.height = h;
+    game.canvas.style.width = w + "px";
+    game.canvas.style.height = h + "px";
+    game.ctx.setTransform(0.99, 0, 0, 0.99, 0, 0);
   }
 }
