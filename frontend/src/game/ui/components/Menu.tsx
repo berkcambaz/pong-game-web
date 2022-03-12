971569d99b1a885e243749ddfaa5a9951d93936e
Berk Cambaz <berkcambaz12321@gmail.com>
2022-03-12 13:00:45 +0300

    Implement main menu

--- frontend/src/game/ui/components/Menu.tsx
+++ frontend/src/game/ui/components/Menu.tsx
@@ -5,5 +5,16 @@ export function Menu() {
   const [_, update] = Soda.state(0);
   game.ui.menuHandler = update;
 
-  return <div>Hello world</div>
+  return (
+    <div class="ui-menu">
+      <div class="title">Pong Online</div>
+      <div class="section">
+        <input type="text" placeholder="Enter id..." />
+        <button>Join</button>
+        <div class="id">
+          Your id: AAAAA
+        </div>
+      </div>
+    </div>
+  )
 }
