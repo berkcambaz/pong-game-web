1905006346f54b83cfc9049182585b878b1afb43
Berk Cambaz <berkcambaz12321@gmail.com>
2022-03-12 15:36:53 +0300

    Update packet tests

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -9,37 +9,23 @@ window.onload = () => {
   game.run();
 }
 
-// Write number
-//const test = new Uint8Array(new Float32Array([1.5]).buffer);
-//let a = new Uint8Array([...test, 123]);
-//console.log(...a);
-
-// Write & read string
-//let string = "ş";
-//let charCodes: number[] = [];
-//for (let i = 0; i < string.length; ++i) charCodes.push(string.charCodeAt(i));
-//let ui32 = new Uint32Array(charCodes);
-//let ui8 = new Uint8Array(ui32.buffer);
-//console.log(...ui32);
-//console.log(...ui8);
-//console.log(String.fromCharCode(new Uint32Array(ui8.buffer)[0]));
-
-const writePacket = new Packet(PACKET_ID.INIT);
-writePacket.writeInt8(127);
-writePacket.writeInt16(32767);
-writePacket.writeInt32(2147483647);
-writePacket.writeBool(false);
-writePacket.writeString("'hello ğış'");
-writePacket.writeFloat32(123.123);
-
-console.log(`${127} ${32767} ${2147483647} ${false} ${"'hello ğış'"} ${123.123}`);
-console.log(...writePacket.writeData);
-
-const readPacket = new Packet(PACKET_ID.INIT, writePacket.writeData);
-const int8 = readPacket.readInt8();
-const int16 = readPacket.readInt16();
-const int32 = readPacket.readInt32();
-const bool = readPacket.readBool();
-const string = readPacket.readString("'hello ğış'".length);
-const float32 = readPacket.readFloat32();
-console.log(`${int8} ${int16} ${int32} ${bool} ${string} ${float32}`);
+/// PACKET TEST FOR READ/WRITE \\\
+//const writePacket = new Packet(PACKET_ID.INIT);
+//writePacket.writeInt8(127);
+//writePacket.writeInt16(32767);
+//writePacket.writeInt32(2147483647);
+//writePacket.writeBool(false);
+//writePacket.writeString("'hello ğış'");
+//writePacket.writeFloat32(123.123);
+//
+//const readPacket = new Packet(PACKET_ID.INIT, writePacket.writeData);
+//const int8 = readPacket.readInt8();
+//const int16 = readPacket.readInt16();
+//const int32 = readPacket.readInt32();
+//const bool = readPacket.readBool();
+//const string = readPacket.readString("'hello ğış'".length);
+//const float32 = readPacket.readFloat32();
+//
+//console.log(`${int8} ${int16} ${int32} ${bool} ${string} ${float32}`);
+//console.log(`${127} ${32767} ${2147483647} ${false} ${"'hello ğış'"} ${123.123}`);
+//console.log(...writePacket.writeData);
