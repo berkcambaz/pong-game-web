064cb6ede3fe08ba59fe087dd390e9fd36209576
Berk Cambaz <berkcambaz12321@gmail.com>
2022-03-13 13:11:43 +0300

    Fix files for production

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,7 +5,7 @@ import { Network } from "./core/network";
 const app = express();
 
 app.use("/", express.static(path.join(__dirname, "../../frontend/public")));
-app.listen(80, () => { console.log("Server has started...") });
+app.listen(process.env.PORT || 80, () => { console.log("Server has started...") });
 
 export const server = {
   network: new Network()
