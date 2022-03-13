4fa8fe83eedd420b1337eecf61ab68c6c85964c3
Berk Cambaz <berkcambaz12321@gmail.com>
2022-03-13 13:16:52 +0300

    Fix pathname for production

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,7 +4,7 @@ import { Network } from "./core/network";
 
 const app = express();
 
-app.use("/", express.static(path.join(__dirname, "../../frontend/public")));
+app.use("/", express.static(path.join(__dirname, "../../../../frontend/public")));
 app.listen(process.env.PORT || 80, () => { console.log("Server has started...") });
 
 export const server = {
